ad90075d5175324c62c1c7195b8c51d5b3cffdd3
Cong Nguyen <kaom.n.92@gmail.com>
2022-12-13 16:19:05 +0200

    Pushing Day 1

--- day01/part1/main.py
+++ day01/part1/main.py
@@ -1,11 +1,9 @@
 fileInput = open('../input.txt', 'r')
 lines = fileInput.readlines()
 
-count = 0
 mostCalories = 0;
 calories = 0;
 for line in lines:
-	count += 1
 	if line.strip() == "":
 		if mostCalories < calories:
 			mostCalories = calories
